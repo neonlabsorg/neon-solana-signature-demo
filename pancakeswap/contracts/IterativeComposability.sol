1be67839d4567fcf0589be18a385e7ade258d5c9
Maxime Suard <maxpolizzo@gmail.com>
2025-02-07 18:16:08 +0100

    WIP: test minting tokens to ATA account using on-chaain formatted instrction

--- pancakeswap/contracts/IterativeComposability.sol
+++ pancakeswap/contracts/IterativeComposability.sol
@@ -11,6 +11,8 @@ contract IterativeComposability {
     bytes32 public constant SYSTEM_PROGRAM_ID = 0x0000000000000000000000000000000000000000000000000000000000000000;
     bytes32 public constant TOKEN_PROGRAM_ID = 0x06ddf6e1d765a193d9cbe146ceeb79ac1cb485ed5f5b37913a8cf5857eff00a9;
 
+    bytes32 public tokenMint;
+
     constructor() {}
 
     function getPayer() public view returns(bytes32) {
@@ -33,9 +35,13 @@ contract IterativeComposability {
         return sha256(abi.encodePacked(basePubKey, seed, TOKEN_PROGRAM_ID));
     }
 
-    function testCreateTokenMintResource(bytes memory seed, uint8 decimals) external payable {
+    function createResource(bytes memory seed, uint64 space, uint64 lamports, bytes32 owner) public returns(bytes32) {
+        return CALL_SOLANA.createResource(keccak256(seed), space, lamports, owner);
+    }
+
+    function testCreateInitializeTokenMint(bytes memory seed, uint8 decimals) external payable {
         // Create SPL token mint account
-        bytes32 tokenMint = CALL_SOLANA.createResource(
+        tokenMint = CALL_SOLANA.createResource(
             keccak256(seed), // salt
             82, // space
             1461600, // lamports
@@ -66,6 +72,25 @@ contract IterativeComposability {
 
         // Execute initializeMint2 instruction
         CALL_SOLANA.execute(0, initializeMint2Ix);
+    }
+
+    function testMintTokens(bytes32 _tokenMint, bytes32 recipientATA, uint64 amount) external payable {
+        // Format mintTo instruction
+        (   bytes32[] memory accounts,
+            bool[] memory isSigner,
+            bool[] memory isWritable,
+            bytes memory data
+        ) = formatMintToInstruction(_tokenMint, recipientATA, amount);
+        // Prepare mintTo instruction
+        bytes memory mintToIx = CallSolanaHelperLib.prepareSolanaInstruction(
+            TOKEN_PROGRAM_ID,
+            accounts,
+            isSigner,
+            isWritable,
+            data
+        );
+        // Execute mintTo instruction
+        CALL_SOLANA.execute(0, mintToIx);
     }
 
     function testIterativeComposability(
@@ -154,7 +179,8 @@ contract IterativeComposability {
         data = abi.encodePacked(
             bytes4(0x03000000), // Instruction variant
             basePubKey, // Base public key used for account  creation
-            bytes8(0x1100000000000000), // Temporary: figure out how to get seed's bytes length in little-endian format
+            // bytes8(0x1100000000000000), // Temporary: figure out how to get seed's bytes length in little-endian format
+            bytes8(convertUintToLittleEndianBytes32(uint256(seed.length))),
             seed,
             bytes8(0x604d160000000000), // Rent exemption balance for created account (1461600 lamports, right-padded little endian)
             bytes8(0x5200000000000000), // Storage space for created account (82 bytes, right-padded little endian)
@@ -189,5 +215,59 @@ contract IterativeComposability {
             bytes1(0x01), // Flag set to 1, indicating that freezeAuthority account is provided next and should be unpacked (see: https://github.com/solana-program/token/blob/08aa3ccecb30692bca18d6f927804337de82d5ff/program/src/instruction.rs#L561)
             freezeAuthority // Token's freeze authority account
         );
+    }
+
+    function formatMintToInstruction(
+        bytes32 tokenMint,
+        bytes32 recipientATA,
+        uint64 amount
+    ) public view returns (
+        bytes32[] memory accounts,
+        bool[] memory isSigner,
+        bool[] memory isWritable,
+        bytes memory data
+    ) {
+        bytes32 mintAuthority = CALL_SOLANA.getNeonAddress(address(this));
+
+        accounts = new bytes32[](1);
+        accounts[0] = tokenMint;
+        accounts[1] = recipientATA;
+        accounts[2] = mintAuthority;
+
+        isSigner = new bool[](1);
+        isSigner[0] = false;
+        isSigner[1] = false;
+        isSigner[2] = true;
+
+        isWritable = new bool[](1);
+        isWritable[0] = true;
+        isWritable[1] = true;
+        isWritable[2] = false;
+
+        data = abi.encodePacked(
+            bytes1(0x07), // Instruction variant (see: https://github.com/solana-program/token/blob/08aa3ccecb30692bca18d6f927804337de82d5ff/program/src/instruction.rs#L508)
+            bytes8(convertUintToLittleEndianBytes32(uint256(amount)))
+        );
+    }
+
+    function convertUintToLittleEndianBytes32(uint256 bigEndian) public pure returns (bytes32) {
+        assembly {
+            // Assign return data position
+            let pos := mload(0x40)
+            // Store little-endian bytes in memory
+            for {let i := 0} lt(i, 32) {i := add(i, 1)} {
+                let nextBEByte := byte(sub(31, i), bigEndian) // Get BE bytes starting from the right
+                let nextLEByte := shl(sub(248, mul(i, 8)), nextBEByte) // Shift left to get LE bytes
+                let currentValue := mload(pos)
+                let newValue := add(currentValue, nextLEByte) // Add LE bytes to return value
+                mstore(pos, newValue) // Update return value in memory
+            }
+            // Assign return data start position
+            let position := mload(0x40)
+            // Update free memory pointer
+            mstore(0x40, add(mload(0x40), 0x20))
+            // Return
+            return(position, 0x20)
+        }
     }
 }
--- pancakeswap/scripts/test-composability.js
+++ pancakeswap/scripts/test-composability.js
@@ -3,7 +3,10 @@ const web3 = require("@solana/web3.js");
 const {
     MINT_SIZE,
     TOKEN_PROGRAM_ID,
-    createInitializeMint2Instruction
+    ASSOCIATED_TOKEN_PROGRAM_ID,
+    createInitializeMint2Instruction,
+    createMintToInstruction,
+    getAssociatedTokenAddress
 } = require('@solana/spl-token');
 const config = require("../config")
 const { asyncTimeout, airdropNEON, getSolanaTransactions, prepareInstruction } = require("./utils");
@@ -46,22 +49,16 @@ async function main() {
 
     const iterativeComposability = await deployIterativeComposabilityContract()
 
+    // =================================== Create and initialize new SPL token mint ====================================
+
     const seed = 'seed' + Date.now().toString();
     const decimals = 9
 
     const contractPublicKeyInBytes = await iterativeComposability.getNeonAddress(iterativeComposability.target);
-    let createWithSeedAccount =  await iterativeComposability.getCreateWithSeedAccount(
-        contractPublicKeyInBytes,
-        Buffer.from(seed)
-    )
-    console.log(createWithSeedAccount, 'createWithSeedAccount')
 
-    let resource =  await iterativeComposability.getResourceAddress(Buffer.from(seed))
-    console.log(resource, 'resource')
+    console.log('\nCalling iterativeComposability.testCreateInitializeTokenMint: ')
 
-    console.log('\nCalling iterativeComposability.testCreateTokenMintResource: ')
-
-    let tx = await iterativeComposability.testCreateTokenMintResource(
+    let tx = await iterativeComposability.testCreateInitializeTokenMint(
         Buffer.from(seed),  // Seed to generate new SPL token mint account on-chain (identical to createAccountWithSeed)
         decimals, // Decimals value for the new SPL token to be created on Solana
     )
@@ -80,8 +77,51 @@ async function main() {
 
     console.log("\n")
 
-    resource =  await iterativeComposability.getResourceAddress(Buffer.from(seed))
-    console.log(resource, 'resource')
+    // =================================== Mint SPL token amount to recipient ====================================
+    const tokenMint = await iterativeComposability.tokenMint();
+    const solanaRecipient = web3.Keypair.generate()
+
+    let solanaRecipientATA = await getAssociatedTokenAddress(
+        new web3.PublicKey(ethers.encodeBase58(tokenMint)),
+        solanaRecipient.publicKey,
+        true
+    );
+
+    console.log(ethers.encodeBase58(tokenMint), 'tokenMint')
+    console.log(solanaRecipient.publicKey.toString(), 'solanaRecipient.publicKey')
+    console.log(solanaRecipientATA.toString(), 'solanaRecipientATA')
+
+    let ix = createMintToInstruction(
+        new web3.PublicKey(ethers.encodeBase58(tokenMint)),
+        solanaRecipientATA,
+        new web3.PublicKey(ethers.encodeBase58(contractPublicKeyInBytes)),
+        1000 * 10 ** 9 // mint 1000 tokens
+    )
+
+    console.log(ix)
+
+    console.log('\nCalling iterativeComposability.testMintTokens: ')
+
+    tx = await iterativeComposability.testMintTokens(
+        tokenMint,
+        solanaRecipientATA.toBuffer(), // Solana recipient ATA
+        1000 * 10 ** 9 // amount (mint 1000 tokens)
+    )
+
+    console.log('\nNeonEVM transaction hash: ' + tx.hash)
+    await tx.wait(1) // Wait for 1 confirmation
+    txReceipt = await ethers.provider.getTransactionReceipt(tx.hash)
+    console.log(txReceipt.status, 'txReceipt.status')
+
+    solanaTransactions = (await (await getSolanaTransactions(tx.hash)).json()).result
+
+    console.log('\nSolana transactions signatures:')
+    for await (let txId of solanaTransactions) {
+        console.log(txId)
+    }
+
+    console.log("\n")
+
 }
 
 async function deployIterativeComposabilityContract() {
